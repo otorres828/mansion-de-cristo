5d6d9d3fee1a4eefc3465d6db37f7db900234b33
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-03-22 13:08:07 -0700

    contactanos

--- resources/views/blog/contactanos.blade.php
+++ resources/views/blog/contactanos.blade.php
@@ -1,71 +1,132 @@
 @extends('layouts.blog')
-@section('title','MDC-Contactanos')
+@section('title', 'MDC-Contactanos')
 
 @section('header')
-    <header data-parallax="true" class="bg-cover bg-center flex items-center relative h-64 py-48 dark-filter" style="background-image: url(&quot;https://aprendible.nyc3.digitaloceanspaces.com/static/persona-programando-480w.jpg&quot;); transform: translate3d(0px, 0px, 0px);"></header>
+    <header data-parallax="true" class="bg-cover bg-center flex items-center relative h-64 py-48 dark-filter"
+        style="background-image: url(&quot;https://aprendible.nyc3.digitaloceanspaces.com/static/persona-programando-480w.jpg&quot;); transform: translate3d(0px, 0px, 0px);">
+    </header>
 @endsection
 
 @section('main')
-<div  class="mr-3 ml-3
+    <div
+        class="mr-3 ml-3
             z-10
             mb-10
             -mt-64
             xl:mx-32
             relative
             rounded-lg
-            bg-gray-100
-            shadow">
-    <div class="pt-5 shadow-lg grid grid-cols-12 gap-10 container mx-auto">
-        <div class="col-span-12 text-center  pt-3 gap-2">
-            <h1 class=" font-bold text-2xl md:text-2xl lg:text-3xl font-heading text-gray-700">
-                Complete el formulario y nos pondremos en contácto lo más pronto posible
-            </h1>
-        </div>
-          
-        <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-7">
-            <div class="bg-white">
-                <form class=" p-10 rounded-lg shadow-lg">
-                    <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Formulario</h1>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nombre</label>                            <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="username" />
+            
+            ">
+        <main>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+                <article class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 py-10">
+
+
+                    <div class="px-6 py-4">
+
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+                            <div class="col-span-1">
+                                <div class="mb-10">
+                                    <h2 class="text-4xl font-bold leading-tight lg:text-5xl mb-2">¡Escríbenos!</h2>
+                                    <div class="text-gray-600">Dejanos un mensaje y te responderemos a la brevedad</div>
+                                </div>
+
+                                <img src="https://codersfree.com/img/contact/doodle.svg" alt="" class="p-6 h-52 md:h-64">
+                            </div>
+
+                            <div class="col-span-1">
+
+                                <div class="flex w-full overflow-hidden bg-white rounded-lg shadow-md">
+                                    <div class="flex-shrink-0 flex items-center justify-center w-12 bg-blue-500">
+                                        <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z">
+                                            </path>
+                                        </svg>
+                                    </div>
+
+                                    <div class="px-4 py-2 -mx-3">
+                                        <div class="mx-3">
+                                            <span class="font-semibold text-blue-500">
+                                                ¡Gracias por escribirnos!
+                                            </span>
+                                            <div class="text-sm text-gray-600">
+                                                Este es un formulario de contácto, no de preguntas y respuestas. Cualquier
+                                                duda sobre mis cursos dejarlo en la sección de comentarios o en el grupo
+                                                Coders Free de facebook.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="https://codersfree.com/contact" method="POST" class="space-y-6 ng-untouched ng-pristine ng-valid">
+                                    @csrf
+                                    <div>
+                                        <label for="name" class="text-sm">Nombre completo</label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                                            name="name" type="text" placeholder="Nombre completo">
+                                    </div>
+                                    <div>
+                                        <label for="email" class="text-sm">Email</label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                                            type="email" placeholder="Escriba aquí su correo electrónico" id="email"
+                                            name="email">
+                                    </div>
+                                    <div>
+                                        <label for="name" class="text-sm">Asunto</label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                                            name="name" type="text" placeholder="Asunto">
+                                    </div>
+                                    <div>
+                                        <label for="message" class="text-sm">Mensaje</label>
+                                        <div class="w-full">
+                                              <textarea
+                                                class="
+                                                  form-control
+                                                  block
+                                                  w-full
+                                                  px-3
+                                                  py-1.5
+                                                  text-base
+                                                  font-normal
+                                                  text-gray-700
+                                                  bg-white bg-clip-padding
+                                                  border border-solid border-gray-300
+                                                  rounded
+                                                  transition
+                                                  ease-in-out
+                                                  m-0
+                                                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                                                "
+                                                id="exampleFormControlTextarea1"
+                                                rows="3"
+                                                placeholder="Your message"
+                                              ></textarea>
+                                          </div>
+                                    </div>
+                                    <div>
+                                        <button class="w-full p-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                            Enviar mensaje
+                                          </button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+
                     </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Correo</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
-                    </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Asunto</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text"  id="password" placeholder="Asunto" />
-                    </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Descripcion</label>
-                        <textarea class="caret-pink-500  w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"  placeholder="Descripcion"></textarea>
-                    </div>
-                    <button type="submit" class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Enviar</button>
-                </form>
+
+                </article>
             </div>
-        </div>       
-       
-        <div class="sm:col-span-12 md:col-span-6 lg:col-span-5 mx-auto">
-            <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg ">
-                <div class="flex justify-center md:justify-end -mt-16">
-                    <img class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="{{asset('images/icons/icon-96x96.png')}}">
-                </div>
-                <div>
-                    <h2 class="text-gray-800 text-3xl font-semibold">Contáctanos</h2>
-                    <p class="mt-2 text-gray-600">Si deseas, también puedes escribirnos a nuestro correo electrónico</p>
-                </div>
-                <div class="flex justify-end mt-4">
-                    <a href="#" class="text-xl font-medium text-indigo-500">olivertorres1997@gmail.com</a>
-                </div>
-            </div>             
-        </div>    
-    </div>    
-</div>
+        </main>
+    </div>
 @endsection
 
 @section('footer')
     @include('components.footerT')
 @endsection
-
-
