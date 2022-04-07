0d03435d00dd095533f7c7b79c1708273f6b576b
Dank23 <100milsatoshis@gmial.com>
2022-04-07 20:06:30 +0800

    bostrap

--- resources/views/components/aminblog/cssjs.blade.php
+++ resources/views/components/aminblog/cssjs.blade.php
@@ -13,9 +13,7 @@
        }
    </style>
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-
 @stop
-
 
 @section('js')
     <script src="{{{ asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}}"></script>
@@ -50,7 +48,7 @@
             reader.readAsDataURL(file);
         }
     </script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 @stop
 
