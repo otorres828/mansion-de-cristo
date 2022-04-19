2d663c6bd803bb541c9afc49f6d885e048389866
Dank23 <100milsatoshis@gmial.com>
2022-04-20 05:06:27 +0800

    prueba

--- resources/views/admin/blog/estadisticas/index.blade.php
+++ resources/views/admin/blog/estadisticas/index.blade.php
@@ -9,9 +9,4 @@
     @livewire('blog.estadisticas',['hoy'=>$hoy])
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.1/css/bulma.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@latest/dist/Chart.min.js"></script>
-
-@stop
 
--- resources/views/livewire/blog/prueba.php
+++ resources/views/livewire/blog/prueba.php
@@ -1,52 +1,48 @@
 <div>
-    <div class="container p-2 md:p-3 lg:p-4">
-        <div class="form-group mt-4">
-            <header class="form-control">
-                <div class="text-bold">
-                    @if ($inicio == $fin)
-                        Estadísticas de {{ $inicio }}
-                    @else
-                        Estadísticas de {{ $inicio }} hasta {{ $fin }}
-                    @endif
-                </div>
-            </header>
-        </div>
+    <section class="section">
+        <header class="card text-center">
+            <div class="card-header-title px-4 ">
+                @if ($inicio == $fin)
+                    Estadísticas de {{ $inicio }}
+                @else
+                    Estadísticas de {{ $inicio }} hasta {{ $fin }}
+                @endif
+            </div>
+        </header>
+
         {{-- RANGO DE FECHAS --}}
-        <div class="form-group">
-            <div >
-                <div class="row align-items-start">
-                    <div class="col">
-                        <label>Desde: </label>
-                        <input class="input form-control" wire:model="inicio" type="date" name="hoy"
-                            value="{{ $inicio }}">
-                    </div>
-                    <div class="col">
-                        <label>Hasta: </label>
-                        <input class="input form-control" wire:model="fin" type="date" name="hoy"
-                            value="{{ $fin }}">
-                    </div>
+        <div class="mb-2">
+            <div class="row align-items-start">
+                <div class="col">
+                    <label>Desde: </label>
+                    <input class="input" wire:model="inicio" type="date" name="hoy" value="{{ $inicio }}">
+                </div>
+                <div class="col">
+                    <label>Hasta: </label>
+                    <input class="input" wire:model="fin" type="date" name="hoy" value="{{ $fin }}">
                 </div>
             </div>
         </div>
-        {{-- tarjetas --}}
-        <div class="row  p-2">
-            <div class="border">
-                <div>
-                    <span class="bg-primary p-2">Visitas</span>
-                    <span class="bg-warning p-2">{{ $visitasYVisitantes->visitas }} </span>
+
+        <div class="field is-grouped is-grouped-multiline">
+
+            <div class="control">
+                <div class="tags has-addons">
+                    <span class="tag is-success is-large">Visitas</span>
+                    <span class="tag is-info is-large">{{ $visitasYVisitantes->visitas }} </span>
                 </div>
             </div>
-            <div class="">
-                <div >
-                    <span class="bg-success p-2">Visitantes</span>
-                    <span class="bg-warning p-2">
+            <div class="control">
+                <div class="tags has-addons">
+                    <span class="tag is-warning is-large">Visitantes</span>
+                    <span class="tag is-info is-large">
                         {{ $visitasYVisitantes->visitantes }}</span>
                 </div>
             </div>
         </div>
 
         {{-- BUSCADOR --}}
-        <div class="pt-2 sm:px-6 lg:px-8">
+        <div class="mx-auto sm:px-6 lg:px-8">
             <input wire:model="buscar"
                 class="mt-10 shadow appearance-none border rounded py-2 form-control text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 type="text" placeholder="ESCRIBE EL NOMBRE DE LA PAGINA QUE BUSCAS">
@@ -54,7 +50,7 @@
 
         {{-- TABLA --}}
         <div class="table-responsive py-4">
-            <table class="table">
+            <table>
                 <thead>
                     <tr>
                         <th>Página</th>
@@ -81,5 +77,5 @@
             </table>
             {{ $paginas->links() }}
         </div>
-    </div>
+    </section>
 </div>
