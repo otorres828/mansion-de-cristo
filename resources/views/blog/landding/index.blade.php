f053c974da44a3648a002329487eebbd30889c79
Dank23 <100milsatoshis@gmial.com>
2022-04-21 07:33:47 +0800

    mejorando landing

--- resources/views/blog/landding/index.blade.php
+++ resources/views/blog/landding/index.blade.php
@@ -50,11 +50,14 @@
             {{-- NOTICIAS --}}
             <div class="max-w-3xl mx-auto text-center mt-8">
                 <h2 class="h1 text-3xl border-l-black mb-4">Puedes Leer las Noticias <a class="text-cyan-900 font-bold"
-                        href="{{ route('blog.testimony') }}">Mas Recientes</a></h2>
-
+                        href="{{ route('blog.testimony') }}">Mas Recientes</a>
+                </h2>
+                <p class="text-xl text-gray-600">
+                    Conoce las noticias de interes mas recientes acerca de nuestra congregacion, si quieres puedes ver  <a class="text-cyan-900 font-bold"
+                        href="{{ route('blog.announces') }}">mas noticias aqui aqui</a>.
+                </p>
             </div>
             <div class="pt-8 sm:px-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
                 @foreach ($announces as $anuncio)
                     <div class="px-6 flex justify-center hover:bg-gray-200">
                         <div class="" class="rounded-lg shadow-lg bg-white ">
@@ -115,59 +118,7 @@
                 </div>
             </section>
 
-            <section class="mt-10">
-                <div class="max-w-6xl mx-auto px-4 sm:px-6">
-                    <div class="pb-12 md:pb-20">
-                        <div class="relative bg-gray-900 rounded py-10 px-8 md:py-16 md:px-12 shadow-2xl overflow-hidden aos-init aos-animate"
-                            data-aos="zoom-y-out">
-                            <div class="absolute right-0 bottom-0 pointer-events-none hidden lg:block" aria-hidden="true">
-                                <svg width="428" height="328" xmlns="http://www.w3.org/2000/svg">
-                                    <defs>
-                                        <radialGradient cx="35.542%" cy="34.553%" fx="35.542%" fy="34.553%" r="96.031%"
-                                            id="ni-a">
-                                            <stop stop-color="#DFDFDF" offset="0%"></stop>
-                                            <stop stop-color="#4C4C4C" offset="44.317%"></stop>
-                                            <stop stop-color="#333" offset="100%"></stop>
-                                        </radialGradient>
-                                    </defs>
-                                    <g fill="none" fill-rule="evenodd">
-                                        <g fill="#FFF">
-                                            <ellipse fill-opacity=".04" cx="185" cy="15.576" rx="16" ry="15.576"></ellipse>
-                                            <ellipse fill-opacity=".24" cx="100" cy="68.402" rx="24" ry="23.364"></ellipse>
-                                            <ellipse fill-opacity=".12" cx="29" cy="251.231" rx="29" ry="28.231"></ellipse>
-                                            <ellipse fill-opacity=".64" cx="29" cy="251.231" rx="8" ry="7.788"></ellipse>
-                                            <ellipse fill-opacity=".12" cx="342" cy="31.303" rx="8" ry="7.788"></ellipse>
-                                            <ellipse fill-opacity=".48" cx="62" cy="126.811" rx="2" ry="1.947"></ellipse>
-                                            <ellipse fill-opacity=".12" cx="78" cy="7.072" rx="2" ry="1.947"></ellipse>
-                                            <ellipse fill-opacity=".64" cx="185" cy="15.576" rx="6" ry="5.841"></ellipse>
-                                        </g>
-                                        <circle fill="url(#ni-a)" cx="276" cy="237" r="200"></circle>
-                                    </g>
-                                </svg>
-                            </div>
-                            <div class="relative flex flex-col lg:flex-row justify-between items-center">
-                                <div class="text-center lg:text-left lg:max-w-xl">
-                                    <h3 class="h3 text-white mb-2">Powering your business</h3>
-                                    <p class="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit nemo expedita voluptas culpa sapiente.</p>
-                                    <form class="w-full lg:w-auto">
-                                        <div
-                                            class="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                                            <input type="email"
-                                                class="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500"
-                                                placeholder="Your email…" aria-label="Your email…"><a
-                                                class="btn text-white bg-blue-600 hover:bg-blue-700 shadow"
-                                                href="#0">Subscribe</a>
-                                        </div>
-                                        <p class="text-sm text-gray-400 mt-3">7 days free trial. No credit card required.
-                                        </p>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            
         </div>
 
     </div>
