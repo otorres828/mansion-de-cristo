a244c22efd57da1de4c79906f302f774985ad76f
Oliver English <52586453+otorres828@users.noreply.github.com>
2022-05-24 15:15:52 -0400

    offline

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,40 @@
-@extends('layouts.app')
 
-@section('content')
+<!DOCTYPE html>
+<html lang="es">
 
-    <h1>You are currently not connected to any networks.</h1>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@endsection
+    <title>Safe Money Venezuela</title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="/css/app.css">
+
+</head>
+
+<body class="leading-normal tracking-normal  m-6 bg-cover bg-fixed"
+        style="background-image: url('https://pagamentossmv.ga/images/header.png');">
+    <div class="container mx-auto">
+        <main class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+            <div class="w-full max-w-sm px-8 pt-6 pb-8 mb-4">
+                <h1
+                    class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+                    Ups!
+                    <span
+                        class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                        Su dispositivo no tiene acceso a internet.
+                    </span>
+                    Por favor vuelva a conectarse a internet!
+                </h1>
+                <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left text-indigo-200">
+                    para que pueda seguir disfrutando de nuestros servicios
+                </p>
+            </div>
+        </main>
+    </div>
+
+    <script src="/js/app.js" defer></script>
+
+</body>
+
+</html>
