2f7e816807a01529185a47644bae432d425fc8f6
otorres828 <olivertorres1997@gmail.com>
2022-05-25 08:15:51 +0800

    haremos lo de estadisticas

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -14,7 +14,6 @@
     @laravelPWA
 </head>
 <body >
-
     <header>
         @include('components.aminblog.navigation')
     </header>
@@ -24,8 +23,8 @@
     @yield('footer')
     <script src="{{ mix('js/app.js') }}"></script>
     @yield('js')
+    @stack('js')
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
-
 </body>
 </html>
--- resources/views/livewire/blog/estadisticas.blade.php
+++ resources/views/livewire/blog/estadisticas.blade.php
@@ -1,5 +1,8 @@
 <div>
     <div class="container p-2 ">
+        <input type="hidden" id="inicio " value="{{ $inicio }}">
+        <input type="hidden" id="fin " value="{{ $fin }}">
+
         <div class="form-group mt-4">
             <header class="form-control">
                 <div class="text-bold">
@@ -61,14 +64,17 @@
                 <tbody>
                     @foreach ($paginas as $pagina)
                         <tr>
-                            <td><a href="{{ $pagina->url }}" class="text-dark">{{ $pagina->pagina }}</a></td>
+                            <td><a href="{{ $pagina->url }}" class="text-dark" id="{{ $pagina->pagina }}}" value="{{ $pagina->pagina }}">{{ $pagina->pagina }}</a></td>
                             <td>{{ $pagina->conteo_visitas }}</td>
                             <td>{{ $pagina->conteo_visitantes }}</td>
                             <td>
-                                <a href="{{ route('admin.blog.estadisticas.mostrar', [$pagina->pagina,$inicio,$fin]) }}"
+                                <a href="{{ route('admin.blog.estadisticas.mostrar', [$pagina->pagina, $inicio, $fin]) }}"
                                     class="btn btn-info">
                                     <i class="fa fa-chart-area"></i>
                                 </a>
+                                <button type="button" class="btn btn-warning">
+                                    <i class="fa fa-chart-area"></i>
+                                    </a>
                             </td>
                         </tr>
                     @endforeach
@@ -77,4 +83,78 @@
             {{ $paginas->links() }}
         </div>
     </div>
+    {{-- MODAL
+    <div class="modal fade" id="pagina{{ $pagina->url }}" tabindex="-1"
+        aria-labelledby="pagina{{ $pagina->url }}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Estadistica de Pagina</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <canvas id="myChart" width="800" height="400"></canvas>
+
+            </div>
+        </div>
+    </div> --}}
+    {{-- @push('js')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@latest/dist/Chart.min.js"></script>
+        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        <script>
+            Livewire.on('mostrarEstadistica', categoriaId => {
+
+                $(document).ready(function() {
+                    $.ajax({
+                        url: "{{ route('admin.blog.estadisticas.mostrar') }}",
+                        dataType: "POST",
+                        data: {
+                            pagina: document.getElementById(`<?php echo $pagina->pagina; ?>`).value,
+                            inicio: document.getElementById("inicio").value,
+                            fin: document.getElementById("fin").value,
+                        },
+
+                    })
+                })
+
+                // const ctx = document.getElementById('myChart');
+                // const cdata = JSON.parse(`<?php echo $data; ?>`)
+                // const myChart = new Chart(ctx, {
+                //     type: 'bar',
+                //     data: {
+                //         labels: cdata.label,
+                //         datasets: [{
+                //             label: 'Cantidad de Visitas',
+                //             data: cdata.data,
+                //             backgroundColor: [
+                //                 'rgba(255, 99, 132, 0.2)',
+                //                 'rgba(54, 162, 235, 0.2)',
+                //                 'rgba(255, 206, 86, 0.2)',
+                //                 'rgba(75, 192, 192, 0.2)',
+                //                 'rgba(255, 159, 64, 0.2)'
+                //             ],
+                //             borderColor: [
+                //                 'rgba(255, 99, 132, 1)',
+                //                 'rgba(54, 162, 235, 1)',
+                //                 'rgba(255, 206, 86, 1)',
+                //                 'rgba(75, 192, 192, 1)',
+                //                 'rgba(153, 102, 255, 1)',
+                //                 'rgba(255, 159, 64, 1)'
+                //             ],
+                //             borderWidth: 1
+                //         }]
+                //     },
+                //     options: {
+                //         scales: {
+                //             y: {
+                //                 beginAtZero: true
+                //             }
+                //         }
+                //     }
+                // });
+
+            })
+        </script>
+    {{-- @endpush --}}
+
+
 </div>
