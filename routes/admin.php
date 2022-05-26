5d75968fd13054739fa5b06da4f7e0d3f7786747
otorres828 <olivertorres1997@gmail.com>
2022-05-26 10:28:07 +0800

    return

--- routes/admin.php
+++ routes/admin.php
@@ -12,32 +12,17 @@ use App\Http\Controllers\Admin\blog\AcercadeController;
 use App\Http\Controllers\Blog\EstadisticaController;
 
 
-// Route::resource('user', UserController::class)->middleware('can:admin.blog.user.index')->except('show')->names('admin.blog.user');
-// Route::resource('category', CategoryController::class)->except('show', 'create', 'edit')->middleware('can:admin.blog.category.index')->except('show')->names('admin.blog.category');
-// Route::resource('anuncios', AnnounceController::class)->middleware('can:admin.blog.announce.index')->except('show')->names('admin.blog.announce');
-// Route::resource('teaching', TeachingController::class)->middleware('can:admin.blog.teaching')->except('show')->names('admin.blog.teaching');
-// Route::resource('ministry', MinistryController::class)->middleware('can:admin.blog.ministry')->except('show')->names('admin.blog.ministry');
-// Route::resource('testimony', TestimonyController::class)->middleware('can:admin.blog.testimony')->except('show')->names('admin.blog.testimony');
-// Route::resource('contact', ContactController::class)->except('store')->middleware('can:admin.blog.contact')->names('admin.blog.contact');
-// Route::get('estadisticas', [EstadisticaController::class, 'index'])->name('admin.blog.estadisticas');
-// Route::get('estadisticas/{pagina}/{inicio}/{fin}/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
-// //Route::any('estadisticas/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
-// Route::any('estadisticas/registrar', [EstadisticaController::class, 'registrar'])->name('admin.blog.estadisticas.registrar');
-// Route::resource('acercade', AcercadeController::class)->middleware('can:admin.blog.acercade')->except('show')->names('admin.blog.acercade');
+Route::resource('user', UserController::class)->middleware('can:admin.blog.user.index')->except('show')->names('admin.blog.user');
+Route::resource('category', CategoryController::class)->except('show', 'create', 'edit')->middleware('can:admin.blog.category.index')->except('show')->names('admin.blog.category');
+Route::resource('anuncios', AnnounceController::class)->middleware('can:admin.blog.announce.index')->except('show')->names('admin.blog.announce');
+Route::resource('teaching', TeachingController::class)->middleware('can:admin.blog.teaching')->except('show')->names('admin.blog.teaching');
+Route::resource('ministry', MinistryController::class)->middleware('can:admin.blog.ministry')->except('show')->names('admin.blog.ministry');
+Route::resource('testimony', TestimonyController::class)->middleware('can:admin.blog.testimony')->except('show')->names('admin.blog.testimony');
+Route::resource('contact', ContactController::class)->except('store')->middleware('can:admin.blog.contact')->names('admin.blog.contact');
+Route::get('estadisticas', [EstadisticaController::class, 'index'])->name('admin.blog.estadisticas');
+Route::get('estadisticas/{pagina}/{inicio}/{fin}/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
+//Route::any('estadisticas/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
+Route::any('estadisticas/registrar', [EstadisticaController::class, 'registrar'])->name('admin.blog.estadisticas.registrar');
+Route::resource('acercade', AcercadeController::class)->middleware('can:admin.blog.acercade')->except('show')->names('admin.blog.acercade');
 
 
-Route::domain('blog.mansiondecristo.ml')->group(function () {
-    Route::resource('user', UserController::class)->middleware('can:admin.blog.user.index')->except('show')->names('admin.blog.user');
-    Route::resource('category', CategoryController::class)->except('show', 'create', 'edit')->middleware('can:admin.blog.category.index')->except('show')->names('admin.blog.category');
-    Route::resource('anuncios', AnnounceController::class)->middleware('can:admin.blog.announce.index')->except('show')->names('admin.blog.announce');
-    Route::resource('teaching', TeachingController::class)->middleware('can:admin.blog.teaching')->except('show')->names('admin.blog.teaching');
-    Route::resource('ministry', MinistryController::class)->middleware('can:admin.blog.ministry')->except('show')->names('admin.blog.ministry');
-    Route::resource('testimony', TestimonyController::class)->middleware('can:admin.blog.testimony')->except('show')->names('admin.blog.testimony');
-    Route::resource('contact', ContactController::class)->except('store')->middleware('can:admin.blog.contact')->names('admin.blog.contact');
-    Route::get('estadisticas', [EstadisticaController::class, 'index'])->name('admin.blog.estadisticas');
-    Route::get('estadisticas/{pagina}/{inicio}/{fin}/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
-    //Route::any('estadisticas/mostrar', [EstadisticaController::class, 'mostrar'])->name('admin.blog.estadisticas.mostrar');
-    Route::any('estadisticas/registrar', [EstadisticaController::class, 'registrar'])->name('admin.blog.estadisticas.registrar');
-    Route::resource('acercade', AcercadeController::class)->middleware('can:admin.blog.acercade')->except('show')->names('admin.blog.acercade');
-      
-});
