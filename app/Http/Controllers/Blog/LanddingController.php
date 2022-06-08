5aecf15fc133c3d44dfd70f2fce1a073e2ab7e44
otorres828 <olivertorres1997@gmail.com>
2022-06-09 04:12:47 +0800

    person

--- app/Http/Controllers/Blog/LanddingController.php
+++ app/Http/Controllers/Blog/LanddingController.php
@@ -14,25 +14,25 @@ class LanddingController extends Controller
 {
     public function index()
     {
-        return view('trabajando');
         $announces = Announce::where('status', 2)
-            ->orderBy('id', 'desc')
+        ->orderBy('id', 'desc')
             ->take(4)
             ->get();
         $teachings = Teaching::where('status', 2)
-            ->orderBy('id', 'desc')
+        ->orderBy('id', 'desc')
             ->take(8)
             ->get();
         $testimonies = Testimony::where('status', 2)
-            ->orderBy('id', 'asc')
+        ->orderBy('id', 'asc')
             ->take(6)
             ->get();
         
-        $fecha = date("Y-m-d");
-        $ip = $_SERVER["REMOTE_ADDR"] ?? "";
-        $url=route('landding.index');
+            $fecha = date("Y-m-d");
+            $ip = $_SERVER["REMOTE_ADDR"] ?? "";
+            $url=route('landding.index');
             DB::select("INSERT INTO visitas(fecha, ip, pagina, url) 
                         VALUES('$fecha', '$ip', 'CASA', '$url')");
+        return view('trabajando');
         return view('welcome', compact('announces','testimonies','teachings'));
     }
 }
