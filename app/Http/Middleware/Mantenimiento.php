b1c408c730a4903002bae7a9437631831d2f84d7
otorres828 <olivertorres1997@gmail.com>
2022-06-17 07:40:36 +0800

    mantenimiento

--- app/Http/Middleware/Mantenimiento.php
+++ app/Http/Middleware/Mantenimiento.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\EmailSend;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -12,8 +13,21 @@ class Mantenimiento
   
     public function handle(Request $request, Closure $next)
     {
-        if(EmailSend::where('name','Mantenimiento')->first()->status==1)
-            return redirect()->route('mantenimiento');
+        if(auth()->check()){
+            $user = User:: find(auth()->user()->id);  
+            $roles = $user->getRoleNames();
+            $variable = 0;
+            foreach ($roles as $rol) {
+                if ($rol == 'Admin Blog' || $rol == 'Master') {
+                    $variable++;   
+                }
+            }
+            if($variable==0)
+                if(EmailSend::where('name','Mantenimiento')->first()->status==1)
+                    return redirect()->route('mantenimiento');
+        }else
+            if(EmailSend::where('name','Mantenimiento')->first()->status==1)
+                return redirect()->route('mantenimiento');
         return $next($request);
     }
 }
