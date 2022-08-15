f7b1128848c386529ffafb8af0a98ee01f9f1100
Oliver Torres <olivertorres1997@gmail.com>
2022-08-16 03:04:16 +0800

    editar acceso

--- resources/views/admin/secretary/user/index.blade.php
+++ resources/views/admin/secretary/user/index.blade.php
@@ -111,8 +111,9 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="staticBackdropLabel">Editar Acceso de Usuario</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
                                         </div>
                                         <div class="modal-body">
                                             @livewire('admin.banned', ['user' => $user])
