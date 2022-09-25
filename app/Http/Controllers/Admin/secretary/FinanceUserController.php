875c2c197c7521b14cb33c0e1c5c2e8874c92b96
Jesus Alfonzo <jesusalfonzo97@gmail.com>
2022-09-25 11:50:16 -0400

    vainitas

--- app/Http/Controllers/Admin/secretary/FinanceUserController.php
+++ app/Http/Controllers/Admin/secretary/FinanceUserController.php
@@ -8,33 +8,34 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 class FinanceUserController extends Controller
-{    public function index()
+{
+    public function index()
     {
-        $user = User:: find(auth()->user()->id);  
+        $user = User::find(auth()->user()->id);
         $roles = $user->getRoleNames();
-        if($roles[0]=='Master'){
-            $finances = Finance::where('temple_id',$user->temple_id)
-                                ->where('financeable_type',User::class)->get();
-        }else{
-            $finances = Finance::where('financeable_id',$user->id)
-            ->where('financeable_type',User::class)->get();            
+        if ($roles[0] == 'Master') {
+            $finances = Finance::where('temple_id', $user->temple_id)
+                ->where('financeable_type', User::class)->get();
+        } else {
+            $finances = Finance::where('financeable_id', $user->id)
+                ->where('financeable_type', User::class)->get();
         }
-        return view('admin.secretary.finance.user.index',compact('finances',));
+        return view('admin.secretary.finance.user.index', compact('finances',));
     }
 
 
-   
+
     public function store(Request $request)
     {
         $request->validate([
-            'amount'=>'required',
-            'reference'=>'required',
-            'date'=>'required',
-         ]); 
-         $finance= Finance::create([
+            'amount' => 'required',
+            'reference' => 'required',
+            'date' => 'required',
+        ]);
+        $finance = Finance::create([
             'amount' => $request['amount'],
             'reference' => $request['reference'],
-            'method_pay'=>$request['method_pay'],
+            'method_pay' => $request['method_pay'],
             'type_finance' => $request['type_finance'],
             'date' => $request['date'],
             'status' => $request['status'],
@@ -42,34 +43,63 @@ class FinanceUserController extends Controller
             'financeable_type' => 'App\Models\User',
             'temple_id' =>  $request['temple_id'],
 
-        ]); 
+        ]);
 
-        return redirect()->route('admin.secretary.finance.user.index')->with('info','la finanza se creo con exito');    
+        return redirect()->route('admin.secretary.finance.user.index')->with('info', 'la finanza se creo con exito');
     }
 
- 
+
     public function show($id)
     {
         //
     }
 
-   
+
     public function edit($id)
     {
         //
     }
 
-   
+
     public function update(Request $request, $id)
     {
-        //
+        return $request->all();
+        $finanza = Finance::find($id);
+
+        $validated = $request->validate([
+            'amount' => 'required',
+            'reference' => 'required',
+            'method_pay' => 'required',
+            'type_finance' => 'required',
+            'date' => 'required',
+            'status' => 'required',
+            'financeable_id' => 'required',
+            'financeable_type' => 'required',
+            'temple_id' => 'required'
+        ]);
+        $finanza->update($request->all());
+        return redirect()->back()->with('info', 'Finanza actualizada con exito');
     }
 
- 
-    public function destroy( $finance)
+    public function destroy($finance)
     {
         $finanza = Finance::find($finance);
-         $finanza->delete();
-        return redirect()->route('admin.secretary.finance.user.index')->with('delete','La finanza se elimino con exito');
+        $finanza->delete();
+        return redirect()->route('admin.secretary.finance.user.index')->with('delete', 'La finanza se elimino con exito');
+    }
+
+    //- Por celulas
+    public function por_celula()
+    {
+        $user = User::find(auth()->user()->id);
+        $roles = $user->getRoleNames();
+        if ($roles[0] == 'Master') {
+            $finances = Finance::where('temple_id', $user->temple_id)
+                ->where('financeable_type', User::class)->get();
+        } else {
+            $finances = Finance::where('financeable_id', $user->id)
+                ->where('financeable_type', User::class)->get();
+        }
+        return view('admin.secretary.finance.celula.index', compact('finances',));
     }
 }
