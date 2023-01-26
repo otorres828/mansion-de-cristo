8f52f8e037b078f4a25c68b805dbdfcdb2cbda64
Oliver Torres <olivertorres1997@gmail.com>
2023-01-26 10:36:40 -0400

    cambiar search

--- app/Http/Livewire/Blog/SearchTeachings.php
+++ app/Http/Livewire/Blog/SearchTeachings.php
@@ -11,7 +11,14 @@ class SearchTeachings extends Component
 
     public function render()
     {
+        $teachings= Teaching::search($this->search)->where('status',2)->get();
+        $h =0;
+        if ($teachings->count() == 1) $h=11;
+        else if($teachings->count() == 2) $h=22; 
+        elseif($teachings->count() == 3) $h=33;
+        elseif($teachings->count() > 3) $h=40; 
+
         return view('livewire.blog.search-teachings',[
-        'teachings' => Teaching::search($this->search)->where('status',2)->get()]);
+        'teachings' => $teachings,'h'=>$h]);
     }
 }
--- resources/views/livewire/blog/search-teachings.blade.php
+++ resources/views/livewire/blog/search-teachings.blade.php
@@ -1,46 +1,29 @@
 <div class="relative ">
     <span class="absolute inset-y-0 left-0 flex items-center pl-3">
         <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path
+                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
         </svg>
-    </span>       
-    <input class=" py-1 border-solid border-1 border-light-blue-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md  w-full pl-10" wire:model="search" type="text" placeholder="Escribe una frase relacionada con la enseñanza que estas buscando">
+    </span>
+    <input
+        class=" py-1 border-solid border-1 border-light-blue-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md  w-full pl-10"
+        wire:model="search" type="text"
+        placeholder="Escribe una frase relacionada con la enseñanza que estas buscando">
 
-    @if($search!=null && $search!=' ' && $teachings->count())
-        <div class="shadow-2xl w-full rounded px-3 py-3 pb-0 absolute z-20" style="background-color: white">
-            @foreach($teachings as $teaching)
-                <ul class=" max-h-screen  bottom-auto text-sm pb-2 scroll-auto" >
-                    <li>
-                        <a href="{{route('blog.show_teaching',$teaching->slug)}}" 
+    @if ($search != null && $search != ' ' && $teachings->count())
+    <div class="shadow-2xl w-full rounded px-3 py-3 pb-0 absolute z-20" style="background-color: white">
+        <ul
+            class="bottom-auto text-sm pb-2 overflow-y-scroll h-{{$h}}">
+                @foreach ($teachings as $teaching)
+                <li class="py-1 hover:bg-slate-200">
+                    <a href="{{ route('blog.show_teaching', $teaching->slug) }}"
                             class="block font-sans font-semibold px-2 py-1 text-gray-600 hover:bg-primary 
                                 hover:text-purple-800 leading-5">
                             {{ $teaching->name }}</a>
-                    </li>   
-                </ul>
-            @endforeach
+                    </li>
+                @endforeach
+            </ul>
         </div>
     @endif
 </div>
-
-{{-- <div class="row ">
-    <div class="col-md-12 z-20">
-        <div class="flex-shrink-0 hidden" style="display: block;">
-            <input wire:model="search" type="text" class="form-control" id="search">
-                @if($search!=null && $search!=' ')
-                    <div class="shadow rounded px-3 py-3 pb-0 absolute" style="background-color: white">
-                        @foreach($teachings as $teaching)
-                            <ul class=" max-h-screen  bottom-auto text-sm pb-2" >
-                                <li>
-                                    <a href="{{route('blog.show_teaching',$teaching->slug)}}" 
-                                        class="block font-sans font-semibold px-2 py-1 
-                                        text-gray-600 hover:bg-primary hover:text-purple-800 leading-5">
-                                        {{ $teaching->name }}
-                                    </a>
-                                </li>   
-                            </ul>
-                        @endforeach
-                    </div>
-                @endif
-        </div>
-    </div>    
-</div> --}}
