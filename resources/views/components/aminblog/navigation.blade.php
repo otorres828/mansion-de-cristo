1665ab89725729ee379f1653d3587f9a62d07e60
Oliver Torres <olivertorres1997@gmail.com>
2023-02-02 19:08:17 -0400

    fixed nabvar

--- resources/views/components/aminblog/navigation.blade.php
+++ resources/views/components/aminblog/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 top-0 z-[200] sticky">
+<nav x-data="{ open: false }" class="fixed min-w-full bg-white border-b border-gray-100 top-0 z-[200]">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
 
