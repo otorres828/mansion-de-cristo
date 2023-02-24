1f47a3d047adc9847909888f6bb3f56802695439
Oliver Torres <olivertorres1997@gmail.com>
2023-02-23 20:04:26 -0400

    cambiar dro

--- resources/views/admin/partiels/drowdown.blade.php
+++ resources/views/admin/partiels/drowdown.blade.php
@@ -15,9 +15,9 @@
         <a class="dropdown-item" href="{{ route('celula_miembro',$user->id) }}">Ver Celulas</a>
         <a class="dropdown-item" href="#">Ver Detalles</a>
         <a class="dropdown-item" data-bs-toggle="modal"href="#"
+        data-bs-target="#cambiar{{ $user->id }}" data-bs-whatever="@mdo">Cambiar Cobertura</a>
+        <a class="dropdown-item" data-bs-toggle="modal"href="#"
         data-bs-target="#eliminar{{ $user->id }}" data-bs-whatever="@mdo">Eliminar
         Registro</a>
-        <a class="dropdown-item" data-bs-toggle="modal"href="#"
-        data-bs-target="#cambiar{{ $user->id }}" data-bs-whatever="@mdo">Cambiar Cobertura</a>
     </div>
 </div>
