52850188c91e0918a0fe41097f80f467d6b282f1
Oliver Torres <olivertorres1997@gmail.com>
2023-02-24 11:08:58 -0400

    si existe

--- app/Http/Controllers/Admin/secretary/CelulaController.php
+++ app/Http/Controllers/Admin/secretary/CelulaController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin\Secretary;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CelulaRequest;
 use App\Models\Celula;
 use App\Models\Celulas_usuario;
 use App\Models\User;
