a405c68faaaeaba86e7f328d09f57f698098e2f6
Oliver Torres <olivertorres1997@gmail.com>
2023-03-04 19:56:08 -0400

    realizado

--- resources/views/admin/secretary/crecimiento/index.blade.php
+++ resources/views/admin/secretary/crecimiento/index.blade.php
@@ -182,6 +182,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
 @stop
 @section('js')
+    <x-delete />
     <x-scrip-table-blog />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
---
+++ resources/views/components/delete.blade.php
@@ -0,0 +1,25 @@
+<script>
+    $('.destroy').submit(function(e) {
+        e.preventDefault();
+        Swal.fire({
+            title: '¿Estas Seguro?',
+            text: "que quieres eliminar este contenido? se perderan los registros de usuarios asociados",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar'
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire(
+                // 'Eliminado!',
+                // 'La red se ha eliminado con exito',
+                // 'success'
+                // )
+                this.submit();
+            }
+        })
+    });
+</script>
