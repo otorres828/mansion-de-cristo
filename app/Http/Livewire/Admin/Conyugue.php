39085440e5dae3b26efd00b36877f47bab81cf23
Oliver Torres <olivertorres1997@gmail.com>
2023-03-22 23:11:31 -0400

    modal enviar solicitud de conyugue

--- app/Http/Livewire/Admin/Conyugue.php
+++ app/Http/Livewire/Admin/Conyugue.php
@@ -3,18 +3,44 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
+use App\Models\VisitaPendiente;
 use Livewire\Component;
 
 class Conyugue extends Component
 {
-    public $conyugue=null;
-    
+    public $conyugue=null,$user=null;
+    public $codigo,$visitas_pendientes,$ce_visitadas;
+    public $yo;
+    public $showModal=false;
+
     public function render()
     {
+        $this->yo=auth()->user();
         $user=auth()->user();
         if ($user->conyugue) {
             $this->conyugue=User::find($user->conyugue);
         }
         return view('livewire.admin.conyugue');
+    }
+
+    public function enviar(){
+        $this->user=User::where('codigo',$this->codigo)->first();
+        if(!$this->user || $this->codigo=='')
+            session()->flash('status',"Usuario no existe");
+        else if ($this->user && $this->user->conyugue)
+            session()->flash('status',"El usuario ya tiene conyugue");
+        else{
+            if($this->user->genero==$this->yo->genero)
+                session()->flash('status',"El usuario que selecciono tiene el mismo genero que usted");
+            else{
+                $this->visitas_pendientes=VisitaPendiente::where('user_id',$this->user->id)->where('estatus',1)->count();
+                $this->ce_visitadas=VisitaPendiente::where('user_id',$this->user->id)->where('estatus',2)->count();
+                $this->showModal=true;
+            }
+        }
+    }
+
+    public function save(){
+        
     }
 }
--- resources/views/livewire/admin/conyugue.blade.php
+++ resources/views/livewire/admin/conyugue.blade.php
@@ -3,9 +3,9 @@
         <div class="px-4 sm:px-0">
             <h3 class="text-lg font-medium text-gray-900">Información de Contugue</h3>
             @if ($conyugue)
-            <p class="mt-1 text-sm text-gray-600">
-                Visualice la informacion de su conyugue
-            </p>
+                <p class="mt-1 text-sm text-gray-600">
+                    Visualice la informacion de su conyugue
+                </p>
             @else
                 <p class="mt-1 text-sm text-gray-600">
                     Añada a su conyugue ingresando el codigo de usuario de este
@@ -17,58 +17,79 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         @if ($conyugue)
-        <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
-            <div class="grid grid-cols-6 gap-6">
-                <div class="col-span-6 sm:col-span-4">
-                    <x-jet-label value="Nombre del Conyugue"
-                        class="text-justify" />
-                    <x-jet-input id="name" type="text" class="mt-2 block w-full " disabled
-                        value="{{$conyugue->name}}" />
-                        <x-jet-label value="Correo del Conyugue"
-                        class="text-justify mt-2" />
-                    <x-jet-input id="name" type="text" class="mt-2 block w-full " disabled
-                        value="{{$conyugue->email}}" />
-                
+            <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label value="Nombre del Conyugue" class="text-justify" />
+                        <x-jet-input type="text" class="mt-2 block w-full " disabled value="{{ $conyugue->name }}" />
+                        <x-jet-label value="Correo del Conyugue" class="text-justify mt-2" />
+                        <x-jet-input type="text" class="mt-2 block w-full " disabled
+                            value="{{ $conyugue->email }}" />
+                    </div>
                 </div>
             </div>
-        </div>
         @else
-            <form>
+            <div>
                 <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
                     <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6 sm:col-span-4">
-                            <x-jet-label value="Ingresar Codigo del Conyugue"
-                                class="text-justify" />
-                            <x-jet-input id="name" type="text" class="mt-2 block w-full"
-                                wire:model.defer="state.name" autocomplete="name" />
-                            <x-jet-input-error for="name" class="mt-2 uppercase" />
+                            <x-jet-label value="Ingresar Codigo del Conyugue" class="text-justify" />
+                            <x-jet-input type="text" class="mt-2 block w-full uppercase" wire:model="codigo" />
                             <x-jet-label class="mt-4"
-                                value="Tenga presente que al agregar su conyugue, este debera de aceptar la solicitud de conyugue y una vez aceptado, todos los discipulos y celulas pasaran a ser directamente del conyugue con el genero 'Hombre'. Estos cambios son irreversibles." />
-
+                                value="Tenga presente que al agregar su conyugue, este debera de aceptar la solicitud de conyugue y una vez aceptado, todos los discipulos y celulas oficiales y evangelisticas pasaran a ser directamente del conyugue con el genero 'Hombre'. Estos cambios son irreversibles." />
                         </div>
                     </div>
                 </div>
 
                 <div
                     class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
-                    <div x-data="{ shown: false, timeout: null }" x-init="window.livewire.find('ao48vqtNoQVveU5MqKjF').on('saved', () => {
-                        clearTimeout(timeout);
-                        shown = true;
-                        timeout = setTimeout(() => { shown = false }, 2000);
-                    })"
-                        x-show.transition.out.opacity.duration.1500ms="shown"
-                        x-transition:leave.opacity.duration.1500ms="" style="display: none;"
-                        class="text-sm text-gray-600 mr-3">
-                        Guardado.
-                    </div>
+                    @if (session('status'))
+                        <div class="mb-4 font-bold text-md text-red-700 pr-5">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-                    <button type="submit"
-                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"
-                        wire:loading.attr="disabled" wire:target="photo">
-                        Guardar
-                    </button>
+                    <x-jet-button wire:click="enviar" wire:loading.attr="disabled">
+                        Enviar Solicitud
+                    </x-jet-button>
+
+                    <x-jet-dialog-modal wire:model="showModal">
+                        <x-slot name="title" class="text-center">
+                            INFORMACION DE CONYUGUE
+                        </x-slot>
+                        <x-slot name="content">
+                            <div class="px-2 mb-3 last:mb-0">
+                                <ul class="space-y-1 mt-2">
+                                    <li class="py-3 rounded-lg bg-white text-gray-600 text-left">
+                                        @if ($user)
+                                            <p><span class="font-bold">Nombre:</span> {{ $user->name }}</p>
+                                            <p><span class="font-bold">Correo:</span> {{ $user->email }}</p>
+                                            <p><span class="font-bold">Genero:</span> {{ $user->genero=='M' ? 'MUJER' : 'HOMBRE' }}</p>
+                                            <p><span class="font-bold">Equipo:</span> {{ count($user->descendants) }} personas</p>
+                                            <p><span class="font-bold">Celulas Oficiales:</span> {{ count($user->recursiveCelulasTodas) }}</p>
+                                            <p><span class="font-bold">Celulas Evangelisticas:</span> {{ count($user->recursiveEvangelisticasTodas) }} </p>
+                                            <p><span class="font-bold">Visitas Pendientes CE:</span> {{ $visitas_pendientes }} </p>
+                                            <p><span class="font-bold">CE Visitadas:</span> {{ $ce_visitadas }} </p>
+
+                                        @endif
+                                    </li>
+                                </ul>
+                            </div>
+                        </x-slot>
+                        <x-slot name="footer">
+                            <x-jet-secondary-button wire:click="$set('showModal', false)" wire:loading.attr="disabled">
+                                {{ __('Close') }}
+                            </x-jet-secondary-button>
+
+                            <x-jet-button class="ml-2" wire:click="save" wire:loading.attr="disabled">
+                                {{ __('Enviar Solicitud') }}
+                            </x-jet-button>
+                        </x-slot>
+                    </x-jet-dialog-modal>
+
                 </div>
-            </form>
+            </div>
         @endif
     </div>
+
 </div>
