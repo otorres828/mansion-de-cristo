d9d49e238999c006da9177c5319ac767013a3528
Oliver Torres <olivertorres1997@gmail.com>
2023-03-31 12:59:02 -0400

    errorsaso

--- app/Http/Controllers/Blog/MinisteryController.php
+++ app/Http/Controllers/Blog/MinisteryController.php
@@ -14,10 +14,10 @@ class MinisteryController extends Controller
         return view('blog.ministery.index');
     }
 
-    public function show(Ministry $ministerio){
-        $this->authorize('publicado',$ministerio); 
+    public function show(Ministry $ministery){
+        $this->authorize('publicado',$ministery); 
         $similares = Ministry::where('status',2)
-                            ->where('id','!=',$ministerio->id)
+                            ->where('id','!=',$ministery->id)
                             ->take(4)
                             ->latest('id')
                             ->get();
@@ -28,9 +28,9 @@ class MinisteryController extends Controller
 
         $fecha = date("Y-m-d");
         $ip = $_SERVER["REMOTE_ADDR"] ?? "";
-        $url=route('blog.show_ministery',$ministerio);
+        $url=route('blog.show_ministery',$ministery);
         DB::select("INSERT INTO visitas(fecha, ip, pagina, url) 
-                   VALUES('$fecha', '$ip', '$ministerio->name', '$url')");
+                   VALUES('$fecha', '$ip', '$ministery->name', '$url')");
                    
         return view('blog.ministery.show',compact('ministery','similares','testimonios'));
     }
